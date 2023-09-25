d66371f1ac06a1011bad98f792d69575b86bd4f9
rey1711 <145863352+rey1711@users.noreply.github.com>
2023-09-25 21:07:47 +0800

    Add files via upload

---
+++ postest.py
@@ -0,0 +1,31 @@
+Registrasi = input("Selamat Datang Mahasiswa Sistem Informasi")
+Nama = input("Masukkan Nama : ")
+Nim = input("Masukkan Nim : ")
+Password = input("Masukkan Password : ")
+
+if Password == "121212": 
+    print("Anda  berhasil  login")
+else : 
+    print("Password yang anda masukan salah")
+
+Angka= float(input("Angka dalam Kilogram : "))
+operasi = input("gram, pon, ons : ")
+
+if operasi == "gram":
+    hasil = Angka * 1000 
+    print (hasil)
+elif operasi == "pon":
+    hasil = Angka * 2.20462
+    print (hasil)
+elif operasi == "ons":
+    hasil= Angka * 10 
+    print (hasil,)
+else :
+    print ("hasil tidak diketahui")
+    
+
+
+
+
+
+
